a8618fa432f925cd4d25a79ff8238c754d7fd74a
mousetrap0 <james0619@naver.com>
2022-09-14 22:08:44 +0900

    rollback

--- src/main/java/co/kr/airport/bbs/dto/param/CreateBbsParam.java
+++ src/main/java/co/kr/airport/bbs/dto/param/CreateBbsParam.java
@@ -8,25 +8,11 @@ public class CreateBbsParam {
     private String id;
     private String title;
     private String content;
-    private String facilityground;
-    private String facilityname;
-    private String breaktime;
-    private String recoverytime;
-    private String breakmanager;
-    private String breakreason;
-    private String writer;
 
     public CreateBbsParam(CreateBbsRequest req) {
         this.id = req.getId();
         this.title = req.getTitle();
         this.content = req.getContent();
-        this.facilityground = req.getFacilityGround();
-        this.facilityname = req.getFacilityName();
-        this.breaktime = req.getBreakTime();
-        this.recoverytime = req.getRecoveryTime();
-        this.breakmanager = req.getBreakManager();
-        this.breakreason = req.getBreakReason();
-        this.writer = req.getWriter();
     }
 
     public Integer getSeq() {
@@ -59,54 +45,5 @@ public class CreateBbsParam {
 
     public void setContent(String content) {
         this.content = content;
-    }
-    public String getFacilityground() {
-        return facilityground;
-    }
-
-    public void setFacilityground(String facilityground) {
-        this.facilityground = facilityground;
-    }
-    public String getFacilityname() {
-        return facilityname;
-    }
-
-    public void setFacilityname(String facilityname) {
-        this.facilityname = facilityname;
-    }
-    public String getBreaktime() {
-        return breaktime;
-    }
-
-    public void setBreaktime(String breaktime) {
-        this.breaktime = breaktime;
-    }
-    public String getRecoverytime() {
-        return recoverytime;
-    }
-    
-    public void setRecoverytime(String recoverytime) {
-        this.recoverytime = recoverytime;
-    }
-    public String getBreakmanager() {
-        return breakmanager;
-    }
-    
-    public void setBreakmanager(String breakmanager) {
-        this.breakmanager = breakmanager;
-    }
-    public String getBreakreason() {
-        return breakreason;
-    }
-    
-    public void setBreakreason(String breakreason) {
-        this.breakreason = breakreason;
-    }
-    public String getWriter() {
-        return writer;
-    }
-    
-    public void setWriter(String writer) {
-        this.writer = writer;
     }
 }
